f61d8fe107db085362392bad4ac29cfcccae49c9
IlarionHalushka <hilarion.galushka@gmail.com>
2019-09-01 23:09:24 +0300

    feat: add helmet(), cors(), logger middlewares

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CustomersController } from './customers.controller';
+import { logger } from './logger-middleware';
 
 @Module({})
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(helmet(), cors(), logger)
+      .forRoutes(CustomersController);
+  }
+}
